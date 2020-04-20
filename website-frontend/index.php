85efaded7de9bc86397ad7ff2f79544823ddce30
d3xt3r0 <prathapss87@gmail.com>
2020-04-20 21:58:41 +0530

    Design Tweaks

--- website-frontend/index.php
+++ website-frontend/index.php
@@ -8,9 +8,19 @@
 	<?php
 		$json = file_get_contents('http://python-api/api/requests/v1/profile');
 		$obj = json_decode($json);
-		echo "<img height=80 width=80 src=$obj></img>";
-
-		echo "<h1> My Repositories </h1> \n";
+		echo "<img class=center height=180 width=180 src=$obj></img>";
+	?>
+<h1 align=center> Prathap S S </h1>
+<hr>
+<div class=column-left>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+	</p>
+</div>
+<div class=column-right>
+	<h1> My Repositories </h1> 
+</div>
+	<?php
 		$json1 = file_get_contents('http://python-api/api/requests/v1/github');
 		$obj1 = json_decode($json1);
 
