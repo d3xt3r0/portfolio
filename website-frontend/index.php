571b09a6e5a7b96ff623b99e19e1c83b3b7114d1
d3xt3r0 <prathapss87@gmail.com>
2020-04-22 20:55:21 +0530

    Tweak and Images directory

--- website-frontend/index.php
+++ website-frontend/index.php
@@ -5,21 +5,21 @@
 	<link rel="stylesheet" href="styles.css">
 </head>
 <body>
-	<?php
-		$json = file_get_contents('http://python-api/api/requests/v1/profile');
-		$obj = json_decode($json);
-		echo "<img class=center height=180 width=180 src=$obj></img>";
-	?>
-<h1 align=center> Prathap S S </h1>
+	<header class="headerbar">
+		<?php
+			$json = file_get_contents('http://python-api/api/requests/v1/profile');
+			$obj = json_decode($json);
+			echo "<img class=center height=180 width=180 src=$obj></img>";
+		?>
+		<br>
+		<h1 align="center"> Prathap S S </h1>
+	</header>
+	
 <hr>
-<div class=column-left>
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.
 	</p>
-</div>
-<div class=column-right>
 	<h1> My Repositories </h1> 
-</div>
 	<?php
 		$json1 = file_get_contents('http://python-api/api/requests/v1/github');
 		$obj1 = json_decode($json1);
